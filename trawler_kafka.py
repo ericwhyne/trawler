a5f31eacbc1425592d2faf9165ee9b871427ba02
Glen Coppersmith <coppersmith@jhu.edu>
2015-08-19 06:00:50 -0400

    Added some basic kafka support

---
+++ trawler_kafka.py
@@ -0,0 +1,52 @@
+"""
+For generic interaction with a kafka stream.
+"""
+
+from pykafka import KafkaClient
+
+
+try:
+    import ujson as json #Faster for some operations
+except:
+    import json
+
+class TrawlerKafka:
+    def __init__(self, host, port, topic='trawler'):
+        """
+        Connect to a running Kafka instance on `host` and
+        `port`, and return closures around this to easily
+        push tweets to it.
+        """
+
+        kafka_client = KafkaClient(hosts="%s:%s" % (host,port))
+        topic = kafka_client.topics[topic]
+        producer = topic.get_producer()
+        self.client = kafka_client
+        self.topic = topic
+        self.producer = producer
+        
+    def send_individual_tweets( self, tweets):
+        """
+        Send each tweet in `tweets` as an individual Kafka message. 
+        """
+        for tweet in tweets:
+            self.producer.produce([json.dumps(tweet)])
+
+
+    def send_bulk_tweets( self, tweets):
+        """
+        Send `tweets` in bulk as a single kafka message.
+        """
+        self.producer.produce(json.dumps(tweets))
+        
+if __name__ == '__main__':
+    """
+    Run standalone to test
+    """
+
+    trawler_kafka = TrawlerKafka( 'localhost',9092)
+    tweets = [{'text':'tweet tweet'},{'text':'tweety tweet tweet'},{'text':'tweety tweet tweety tweet'}]
+    trawler_kafka.send_individual_tweets(tweets)
+    trawler_kafka.send_bulk_tweets(tweets)
+    trawler_kafka.send_bulk_tweets(tweets)
+
